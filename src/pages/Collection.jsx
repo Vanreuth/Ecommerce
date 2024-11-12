c4371d821c4d4051d5d08b29ba800fc571ecbdae
Heng Vanreuth <hengvanreuth.17@gmail.com>
2024-11-12 21:00:21 +0700

    finished collection

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -9,10 +9,73 @@ const Collection = () => {
   const [filterProduct, setFilterProduct] = useState([]);
   const [category , setCategory] = useState([]);
   const [subCategory , setSubCategory] = useState([]);
+  const [sortType , setSortType] = useState('relavent');
+
+
+  const toggleCategory = (e) => {
+    const value = e.target.value;
+  
+    if (category.includes(value)) {
+      setCategory((prev) => prev.filter((item) => item !== value));
+    } else {
+      setCategory((prev) => [...prev, value]);
+    }
+  };
+  const toggleSubCategory = (e) => {
+    const value = e.target.value;
+  
+    if (category.includes(value)) {
+      setSubCategory((prev) => prev.filter((item) => item !== value));
+    } else {
+      setSubCategory((prev) => [...prev, value]);
+    }
+  };
+
+  const applyFifter = ()=>{
+    let productCopy = products.slice();
+    if(category.length >0){
+      productCopy = productCopy.filter(item =>category.includes(item.category))
+
+    }
+    if(subCategory.length >0){
+      productCopy = productCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+    setFilterProduct(productCopy);
+  }
+
+
+  const sortPrice =()=>{
+     let fpCopy = filterProduct.slice();
+     switch(sortType){
+       case 'low-high':
+        setFilterProduct(fpCopy.sort((a,b)=>(a.price - b.price)));
+        break;
+       case 'high-low':
+        setFilterProduct(fpCopy.sort((a,b)=>(b.price - a.price)));
+        break;
+        default:
+          applyFifter();
+        break;
+        
+     }
+  }
+
+
+  
 
    useEffect(()=>{
     setFilterProduct(products);
    },[])
+
+   useEffect(()=>{
+      applyFifter();
+   },[category,subCategory])
+
+   useEffect(()=>{
+     sortPrice();
+   },[sortType])
+
+  
   return (
     <div className=' flex flex-col sm:flex-row border-t gap-1 sm:gap-10 pt-10'>
 
@@ -27,13 +90,13 @@ const Collection = () => {
            <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
            <div className=' flex flex-col gap-2 text-sm font-light text-gray-700 '>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Men'} />Men
+              <input className=' w-3' type="checkbox" value={'Men'} onChange={toggleCategory} />Men
             </p>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Woman'} />Women
+              <input className=' w-3' type="checkbox" value={'Women'} onChange={toggleCategory} />Women
             </p>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Kids'} />Kids
+              <input className=' w-3' type="checkbox" value={'Kids'} onChange={toggleCategory} />Kids
             </p>
            </div>
         </div>
@@ -43,13 +106,13 @@ const Collection = () => {
            <p className='mb-3 text-sm font-medium'>TYPE</p>
            <div className=' flex flex-col gap-2 text-sm font-light text-gray-700 '>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Topwear'} />Topwear
+              <input className=' w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/>Topwear
             </p>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Bottomwear'} />Bottomwear
+              <input className=' w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory} />Bottomwear
             </p>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Winterwear'} />Winterwear
+              <input className=' w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory} />Winterwear
             </p>
            </div>
         </div>
@@ -65,10 +128,10 @@ const Collection = () => {
 
           <Title text1={'ALL'} text2={'COLLECTION'}/>
           {/* product sort */}
-          <select className=' border-2 border-gray-300 text-sm'>
+          <select onChange={(e)=>setSortType(e.target.value)} className=' border-2 border-gray-300 text-sm'>
              <option value="relavent">Sort by: Relavent</option>
-             <option value="high">Sort by: Low to High</option>
-             <option value="low">Sort by: High to Low</option>
+             <option value="low-high">Sort by: Low to High</option>
+             <option value="high-low">Sort by: High to Low</option>
           </select>
 
         </div>
